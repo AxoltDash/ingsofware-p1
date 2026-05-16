2b986c302984bd055598eac81a0a606f35bfc192
Juanpi003 <126223448+Juanpi003@users.noreply.github.com>
2026-05-15 23:46:22 -0600

    Add files via upload

---
+++ miPrimerProyecto/elProyecto/static/main.js
@@ -0,0 +1,5 @@
+console.log("JavaScript cargado correctamente");
+
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Buenos dias, disfrute del recorrido de las tres paginas que he creado");
+});
---
+++ miPrimerProyecto/elProyecto/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def home(request):
+    return render(request, "elProyecto/home.html")
+
+@login_required
+def siames(request):
+    return render(request, "elProyecto/siames.html")
+
+@login_required
+def creepyNuts(request):
+    return render(request, "elProyecto/creepyNuts.html")
+
+def registro(request):
+
+    data = {
+        'form': CustomUserCreationForm()
+    }
+
+    if request.method == "POST":
+        formulario = CustomUserCreationForm(data=request.POST)
+
+        if formulario.is_valid():
+            usuario = formulario.save()
+
+            user = authenticate(
+                username=formulario.cleaned_data["username"],
+                password=formulario.cleaned_data["password1"]
+            )
+
+            login(request, user)
+
+            return redirect("home")
+
+        data["form"] = formulario
+
+    return render(request, 'registration/registro.html', data)
