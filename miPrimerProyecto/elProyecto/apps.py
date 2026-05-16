76fc52e11284f6c3d0589e1d08adaf5148529c7d
Juanpi003 <126223448+Juanpi003@users.noreply.github.com>
2026-05-15 23:45:15 -0600

    Delete miPrimerProyecto directory

--- miPrimerProyecto/elProyecto/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ElproyectoConfig(AppConfig):
-    name = 'elProyecto'
--- miPrimerProyecto/elProyecto/static/main.js
+++
@@ -1,5 +0,0 @@
-console.log("JavaScript cargado correctamente");
-
-document.addEventListener("DOMContentLoaded", function () {
-    alert("Buenos dias, disfrute del recorrido de las tres paginas que he creado");
-});
