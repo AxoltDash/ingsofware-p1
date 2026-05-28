1f6b526b12242c4af1f93ee636354ab19e5f04ae
aaronlm <aaronlm25@ciencias.unam.mx>
2026-05-28 12:31:23 -0600

    se hizo el llenado de la abse de datos, agreagndo diez parques, la funcionaldiad anterior se mantiene y solo se hace la migracion para el llenado de la abse de datos, el flujo de trabajo es primero hacer migrate en el proeycto para generar las tabals de la BD y luego hacer seed_parques la cual se encargara de llenar la abse de datos de los parques

--- miPrimerProyecto/miPrimerProyecto/settings.py
+++ miPrimerProyecto/miPrimerProyecto/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from pathlib import Path
+from decouple import config
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,9 +138,14 @@ BASE_URL = env('BASE_URL', default='http://127.0.0.1:8000')
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': env("DATABASE_ENGINE"),
-        'NAME': BASE_DIR / env("DATABASE_NAME"),
+    "default": {
+        "ENGINE":   "django.db.backends.mysql",
+        "NAME":     config("DB_NAME"),
+        "USER":     config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST":     config("DB_HOST", default="localhost"),
+        "PORT":     config("DB_PORT", default="3306"),
+        "OPTIONS":  {"charset": "utf8mb4"},
     }
 }
 
