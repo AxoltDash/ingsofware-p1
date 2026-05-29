d82e154b617eef2fa50192bc302af3522158fe23
Aaron Lopez Mendoza <158116914+Aaronlm25@users.noreply.github.com>
2026-05-29 10:50:50 -0600

    Revert "Se agrego un mapa interactivo y la base para el sistema de reservaciones "

--- miPrimerProyecto/elProyecto/views.py
+++ miPrimerProyecto/elProyecto/views.py
@@ -1,58 +1,41 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
-from parques.models import Parque
 from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def home(request):
     return render(request, "elProyecto/home.html")
 
-
-def bosques(request):
-
-    parques_qs = Parque.objects.filter(activo=True)
-
-    parques = list(
-
-        parques_qs.values(
-
-            'nombre',
-            'direccion',
-            'horario',
-            'latitud',
-            'longitud',
-            'tiene_cabanas',
-            'capacidad_camping'
-        )
-    )
-
-    return render(
-
-        request,
-
-        'elProyecto/bosques.html',
-
-        {
-            'parques': parques
-        }
-    )
-
+@login_required
+def siames(request):
+    return render(request, "elProyecto/siames.html")
 
 @login_required
-def reservaciones(request):
-    return render(request, "elProyecto/reservaciones.html")
+def creepyNuts(request):
+    return render(request, "elProyecto/creepyNuts.html")
 
 def registro(request):
-    data = {'form': CustomUserCreationForm()}
+
+    data = {
+        'form': CustomUserCreationForm()
+    }
 
     if request.method == "POST":
         formulario = CustomUserCreationForm(data=request.POST)
+
         if formulario.is_valid():
-            user = formulario.save()
-            user.backend = 'django.contrib.auth.backends.ModelBackend'
+            usuario = formulario.save()
+
+            user = authenticate(
+                username=formulario.cleaned_data["username"],
+                password=formulario.cleaned_data["password1"]
+            )
+
             login(request, user)
+
             return redirect("home")
+
         data["form"] = formulario
 
     return render(request, 'registration/registro.html', data)
